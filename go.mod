a73ff9278cba09b3d29aa7e913c560b2e832db62
ibnu ahmad kamal <ibnu@qiscus.co>
2022-10-25 13:18:06 +0700

    add V2.0.0

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/ibnuahmadkamal/go-modul-contoh
+module github.com/ibnuahmadkamal/go-modul-contoh/v2
 
 go 1.18
--- say-hello.go
+++ say-hello.go
@@ -1,7 +1,7 @@
 package go_say_hello
 
-func SayHello() string {
-	return "hello word"
+func SayHello(name string) string {
+	return "hello " + name
 }
 
 // initiate mod in go using go mod init
